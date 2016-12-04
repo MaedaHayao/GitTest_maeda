08bfae954c1671513a7b5a5e4f2da524c7ddff4a
MaedaHayao <sijvision1010@gmail.com>
2016-12-04 06:38:39 +0000

    make home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 GitTest::Application.routes.draw do
+  get "home/top"
+  get "home/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
